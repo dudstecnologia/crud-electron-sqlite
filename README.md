633d499e7ded5ff34dc158768d3506a952f0a2b4
Eduardo Silva <eduardo@dudstecnologia.com.br>
2019-09-04 03:55:25 -0400

    Database

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # CRUD Electron e Sqlite
 
-Material de estudo
-https://www.techiediaries.com/electron-data-persistence/
+<p>npm install --save-dev electron-rebuild</p>
 
-https://electronjs.org/docs/tutorial/using-native-node-modules
+<p>sudo apt-get -y install g++ // Para Linux</p>
+
+<p>npm install sqlite3 --save</p>
+
+<p>./node_modules/.bin/electron-rebuild  -f -w sqlite3</p>
+
+<p>https://www.techiediaries.com/electron-data-persistence/</p>
+
+<p>https://electronjs.org/docs/tutorial/using-native-node-modules</p>
--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const {app, BrowserWindow} = require('electron')
 const path = require('path')
+const sqlite3 = require('sqlite3');
 
 let mainWindow
 
@@ -20,9 +21,20 @@ function createWindow () {
   })
 
   // ##########################################
+  var db = new sqlite3.Database('./database/db.sqlite');
 
+  db.serialize(function () {
+    /*
+    db.run("CREATE TABLE contatos (id	INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT, nome TEXT NOT NULL, telefone TEXT)");
   
+    db.run("INSERT INTO contatos (nome, telefone) VALUES ('Maria Silva', '092993846453');
+    db.run("INSERT INTO contatos (nome, telefone) VALUES ('Carlos Machado', '011956473645');
+    */
 
+    db.each("SELECT * FROM contatos", function (err, row) {
+      console.log(row);
+    });
+  });
   // ##########################################
 
 }
